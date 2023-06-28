8de70acadca3aca567b67b8aec4d282aeea4c134
amanjn1122 <115038153+amanjn1122@users.noreply.github.com>
2023-06-28 16:04:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -94,4 +94,8 @@ function allowDrop(ev) {
   }
 /////////////////////////
 
-document.addEventListener("contextmenu", e => e.preventDefault());
+document.addEventListener("contextmenu", e => {
+    e.preventDefault();
+    const x=document.querySelector("link");
+    x.remove();
+});
